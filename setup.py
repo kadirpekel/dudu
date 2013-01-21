c51159a04a83352d2551a6620ad1fe27a8f28cb8
kadirpekel <kadirpekel@gmail.com>
2013-01-21 03:04:30 +0200

    fixed setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description='get things done!',
     author='Kadir Pekel',
     author_email='kadirpekel@gmail.com',
-    url='https://github.com/kadirpekel/todo.py',
+    url='https://github.com/kadirpekel/dudu',
     install_requires=[
         'komandr>=0.1.1'
     ],
