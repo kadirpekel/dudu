e569a0b221d0d9783a8072198ffe1e19a47a9870
kadirpekel <kadirpekel@gmail.com>
2013-01-21 08:43:52 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='dudu',
     scripts=['dudu'],
-    version='0.0.1',
+    version='0.0.2',
     description='get things done!',
     author='Kadir Pekel',
     author_email='kadirpekel@gmail.com',
